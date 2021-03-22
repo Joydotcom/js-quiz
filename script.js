59e16a256657921d4f0649f25cc4139d04bd73c8
joydotcom <msjoyjackson@gmail.com>
2021-03-22 16:22:33 -0400

    added html and js files and started html skeleton, as well as adding dom varables to js

---
+++ script.js
@@ -0,0 +1,12 @@
+//dom variables
+var questionsEl = document.getElementById("questions");
+var questionTitleEl = document.getElementById("question-title");
+var questionAnswerChoicesEl = document.getElementById("answer-choices");
+
+// javascript variables
+
+//function definitions
+
+//event listeners
+
+//function calls
