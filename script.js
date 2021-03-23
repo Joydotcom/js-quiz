dd48a2d3a8a593ecc50d360c51347637bda7f6b7
joydotcom <msjoyjackson@gmail.com>
2021-03-23 17:04:22 -0400

    added questions and anaswers to script and html.

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 //dom variables
-var questionsEl = document.getElementById("questions");
-var questionTitleEl = document.getElementById("question-title");
-var questionAnswerChoicesEl = document.getElementById("answer-choices");
+// var questionsEl = document.getElementById("questions");
+// var questionTitleEl = document.getElementById("question-title");
+// var questionAnswerChoicesEl = document.getElementById("answer-choices");
 
 // javascript variables
 var buttonOne =document.getElementById("choices".children[1]);
@@ -10,41 +10,67 @@ var buttonThree =document.getElementById("choices".children[3]);
 var buttonFour =document.getElementById("choices".children[4]);
 var buttons = document.querySelectorAll("button");
 
-correctAnswer = 
 
 const myQuestions = [
         {
-          question: "Who invented JavaScript?",
+          question: "Commonly used data types DO NOT include :",
           answers: {
-            1: "Douglas Crockford",
-            2: "Sheryl Sandberg",
-            3: "Brendan Eich",
-            4: "homie"
+            1: "strings",
+            2: "booleans",
+            3: "alerts",
+            4: "numbers"
           },
           correctAnswer: "3"
         },
         {
-          question: "Which one of these is a JavaScript package manager?",
+          question: "The condition in an if/else statement is enclosed within __________.",
           answers: {
-            1: "Node.js",
-            2: "TypeScript",
-            3: "npm",
-            4:"brother"
+            1: "quotes",
+            2: "curly brackets",
+            3: "parenthesis",
+            4: "square brackets"
 
           },
           correctAnswer: "3"
         },
         {
-          question: "Which tool can you use to ensure code quality?",
+          question: "Arrays in JavaScript can be used to store___________.",
           answers: {
-            1: "Angular",
-            2: "jQuery",
-            3: "RequireJS",
-            4: "ESLint"
+            1: "numbers & strings",
+            2: "other arrays",
+            3: "booleans",
+            4: "all of the above"
           },
           correctAnswer: "4"
         }
+    {"String values must be enclosed within ___________ when being assigned variables."
+        question: 
+        answers: {
+            1: "commas",
+            2: "curly brackets",
+            3: "quotes",
+            4: "parenthesis"
+        },
+        correctAnswer: "3"
+    }
+    {"A very useful tool used during development and debugging for printing content to te debugger is ______________."
+        question: 
+        answers: {
+            1: "JavaScript",
+            2: "terminal / bash",
+            3: "for loops",
+            4: "console log"
+        },
+        correctAnswer: "4"
+    }
       ];
+      
+      questionTitleEl.textContent = questions[0].question;
+questionOne.textContent= "1. " + questions[0].choices[0];
+questionTwo.textContent= "2. " + questions[0].choices[1];
+questionThree.textContent= "3. " + questions[0].choices[2];
+questionFour.textContent= "4. " + questions[0].choices[3];
+
       console.log(myQuestions);
 
 
@@ -54,7 +80,7 @@ const myQuestions = [
 //     questionOne: {
 //         question: "Whats good in your hood?",
 //         answerChoiceOne: "nothing",
-//         answerChoiceTwo: "sometheing",
+//         answerChoiceTwo: "something",
 //         answerChoiceThree: "everything",
 //         answerChoiceFour: "alladat",
 
@@ -66,15 +92,29 @@ const myQuestions = [
 // buttonOne.textContent = questionsAndAnswers[0].questionOne.answerChoiceOne;
 
 // //function definitions
-questionAnswerChoicesEl.apprendChild(buttonOne);
-buttonOne.setAttribute("value","correct-answer");
-console.log(buttonOne)
-
-var .addEventListener("click", function(event){
-    if ("correct-answer"){
-        alert("it worked!")
+questionAnswerChoicesEl.addEventListener("click", function(){
+    if (answers.value = correctAnswer) {
+        alert("correct"); 
+    } else {
+        alert("incorrect");
     }
+    nextQuestion();
 });
+
+
+
+
+
+
+
+// buttonOne.setAttribute("value","correct-answer");
+// console.log(buttonOne)
+
+// var .addEventListener("click", function(event){
+//     if ("correct-answer"){
+//         alert("it worked!")
+//     }
+// }
 
 //   console.log(myQuestions);
 
